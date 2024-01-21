4a917b38c4c7a045a6d76876aceada9936344d8a
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-01-21 16:48:10 +0700

    Test library performance-now

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+
 import log from "./utils/logger";
 import router from "./routes";
 import handlerError from "./middlewares/handler-error.middleware";
@@ -8,7 +9,6 @@ const app = express();
 const port = process.env.PORT || 3500;
 
 app.use(morgan("dev"));
-
 app.use(router);
 app.use(handlerError);
 
