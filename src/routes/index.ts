077b83078261ce4f86f8cbb36a6ff7966906cffa
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-07-25 16:18:51 +0700

    Add Dockerfile

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
-import { Router } from "express";
+import { NextFunction, Request, Router } from "express";
 import health from "./health.routes";
+import { AppRes } from "../types/express";
 
 const router = Router();
+
+router.get("/", (req: Request, res: AppRes, next: NextFunction) => {
+  res.json({ message: "Hello World!" });
+});
 router.use("/health", health);
 
 export default router;
